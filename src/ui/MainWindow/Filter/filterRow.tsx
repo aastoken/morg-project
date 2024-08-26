9807f076b372667a7bead580bdc138992ec7b72f
Alberto Abad <alberto.abad.sanz@gmail.com>
2024-08-26 14:23:50 +0200

    Working form submit on filter

--- src/ui/MainWindow/Filter/filterRow.tsx
+++ src/ui/MainWindow/Filter/filterRow.tsx
@@ -46,108 +46,134 @@ function getTagTypesFromTagArray(tags: Tag[]): TagType[]{
 }
 
 
-export default function FilterRow({ deleteRow }: { deleteRow: () => void }){
+export default function FilterRow({ rowData, deleteRow, updateRow }: { rowData: any, deleteRow: () => void, updateRow: (data: any) => void }) {
 
-  const [selectedKey, setSelectedKey] = useState<string | undefined>(trackKeys[0]);
-  const [selectedComparator, setSelectedComparator] = useState<string>(stringOptions[0]);
-  const [selectedTags, setSelectedTags] = useState<Tag[]>([]) 
-  const [selectedGenres, setSelectedGenres] = useState<Genre[]>([])
+
 
   useEffect(() => {
-    // Set the default comparator based on the selected key
-    setSelectedComparator(dynamicOptions[selectedKey as keyof typeof dynamicOptions]?.[0] || '');
-
-    
-
-  }, [selectedKey]);
+    updateRow({ selectedComparator: dynamicOptions[rowData.selectedKey]?.[0] || '' });
+  }, [rowData.selectedKey]);
 
 
 
   const handleTagSelect = (selectedTag: Tag) => {
-    setSelectedTags((prevSelectedTags) => {
-      const isTagSelected = prevSelectedTags.some(
-        (tag) => tag.typeName === selectedTag.typeName && tag.name === selectedTag.name
-      );
-
-      if (isTagSelected) {
-        return prevSelectedTags.filter(
-          (tag) => !(tag.typeName === selectedTag.typeName && tag.name === selectedTag.name)
-        );
-      } else {
-        return [...prevSelectedTags, selectedTag];
-      }
+    updateRow({
+      selectedTags: rowData.selectedTags.some(tag => tag.name === selectedTag.name)
+        ? rowData.selectedTags.filter(tag => tag.name !== selectedTag.name)
+        : [...rowData.selectedTags, selectedTag]
     });
   };
 
   const handleGenreSelect = (selectedGenre: Genre) => {
-    setSelectedGenres((prevSelectedGenres) => {
-      const isGenreSelected = prevSelectedGenres.some(
-        (genre) => genre.name === selectedGenre.name
-      );
-
-      if (isGenreSelected) {
-        return prevSelectedGenres.filter(
-          (genre) => !(genre.name === selectedGenre.name)
-        );
-      } else {
-        return [...prevSelectedGenres, selectedGenre];
-      }
+    updateRow({
+      selectedGenres: rowData.selectedGenres.some(genre => genre.name === selectedGenre.name)
+        ? rowData.selectedGenres.filter(genre => genre.name !== selectedGenre.name)
+        : [...rowData.selectedGenres, selectedGenre]
     });
   };
   
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    updateRow({ [name]: value });
+  };
 
   const handleInputType = () =>{
     let element :JSX.Element = <></>;
-    if(selectedComparator == 'range' && selectedKey=='Date Added'){
+    if(rowData.selectedComparator == 'range' && rowData.selectedKey=='Date Added'){
       element = 
       <>
-        <input className="w-5/12 h-full pl-1" name="inputValueMin" type="date" placeholder="Minimum date">
-        </input>
-        <input className="w-5/12 h-full pl-1" name="inputValueMax" type="date" placeholder="Maximum date">
-        </input>
-      </>
+          <input
+            className="w-5/12 h-full pl-1"
+            name="inputValueMin"
+            type="date"
+            placeholder="Minimum date"
+            value={rowData.inputValueMin || ''}
+            onChange={handleInputChange}
+          />
+          <input
+            className="w-5/12 h-full pl-1"
+            name="inputValueMax"
+            type="date"
+            placeholder="Maximum date"
+            value={rowData.inputValueMax || ''}
+            onChange={handleInputChange}
+          />
+        </>
     }
-    else if (selectedComparator == 'range'){
+    else if (rowData.selectedComparator == 'range'){
       element = 
       <>
-        <input className="w-5/12 h-full pl-1" name="inputValueMin" type="number" placeholder="Minimum value">
-        </input>
-        <input className="w-5/12 h-full pl-1" name="inputValueMax" type="number" placeholder="Maximum value">
-        </input>
-      </>
+          <input
+            className="w-5/12 h-full pl-1"
+            name="inputValueMin"
+            type="number"
+            placeholder="Minimum value"
+            value={rowData.inputValueMin || ''}
+            onChange={handleInputChange}
+          />
+          <input
+            className="w-5/12 h-full pl-1"
+            name="inputValueMax"
+            type="number"
+            placeholder="Maximum value"
+            value={rowData.inputValueMax || ''}
+            onChange={handleInputChange}
+          />
+        </>
     }
-    else if (numberOptions.includes(selectedComparator) && selectedKey=='Date Added'){
-      element = <input className="w-full h-full pl-1" name="inputValue" type="date" placeholder="Enter value"></input>
+    else if (numberOptions.includes(rowData.selectedComparator) && rowData.selectedKey=='Date Added'){
+      element = <input
+        className="w-full h-full pl-1"
+        name="inputValue"
+        type="date"
+        placeholder="Enter value"
+        value={rowData.inputValue || ''}
+        onChange={handleInputChange}
+      />
     }
-    else if (numberOptions.includes(selectedComparator)){
-      element = <input className="w-full h-full pl-1" name="inputValue" type="number" placeholder="Enter value"></input>
+    else if (numberOptions.includes(rowData.selectedComparator)){
+      element = <input
+        className="w-full h-full pl-1"
+        name="inputValue"
+        type="number"
+        placeholder="Enter value"
+        value={rowData.inputValue || ''}
+        onChange={handleInputChange}
+      />
     }
-    else if (stringOptions.includes(selectedComparator) && selectedKey != 'Genres' && selectedKey != 'Tags'){
-      element = <input className="w-full h-full pl-1" name="inputValue" type="text" placeholder="Enter text"></input>
+    else if (stringOptions.includes(rowData.selectedComparator) && rowData.selectedKey != 'Genres' && rowData.selectedKey != 'Tags'){
+      element = <input
+        className="w-full h-full pl-1"
+        name="inputValue"
+        type="text"
+        placeholder="Enter text"
+        value={rowData.inputValue || ''}
+        onChange={handleInputChange}
+      />
     }
-    else if (tagGenreOptions.includes(selectedComparator) && selectedKey == 'Genres'){
+    else if (tagGenreOptions.includes(rowData.selectedComparator) && rowData.selectedKey == 'Genres'){
       element = <div className="flex w-full max-w-full h-full">
-                  <div className="flex-grow pl-1 content-center overflow-y-auto bg-slate-500" id="inputValue">
-                    <GenresVisualizer genres={selectedGenres} onGenreSelect={handleGenreSelect}/>
+                  <div className="flex-grow pl-1 content-center overflow-y-auto bg-slate-100" id="inputValue">
+                    <GenresVisualizer genres={rowData.selectedGenres} onGenreSelect={handleGenreSelect}/>
                   </div>
                   <Popup trigger={<div className="flex w-1/12 h-full p-1 justify-center bg-amber-300" id="inputValue"><PencilSquareIcon className="w-5"/></div>} 
                   modal
                   >    
-                    <div className="flex flex-col relative justify-start w-[400px] max-h-[400px] top-0 left-1/3  bg-slate-600 border-2 p-1 ">
+                    <div className="flex flex-col relative justify-start w-[400px] max-h-[400px] top-0 left-1/3 bg-slate-600 border-2 p-1 ">
                       <GenreBrowser onGenreSelect={handleGenreSelect}/>
                     </div>
                   </Popup>
                 </div>
     }
-    else if (tagGenreOptions.includes(selectedComparator) && selectedKey == 'Tags'){
+    else if (tagGenreOptions.includes(rowData.selectedComparator) && rowData.selectedKey == 'Tags'){
       element = <div className="flex w-full max-w-full h-full">
-                  <div className="flex-grow pl-1 align-middle overflow-y-auto bg-slate-500" id="inputValue">
-                    <TagTypesVisualizer tag_types={getTagTypesFromTagArray(selectedTags)} onTagSelect={handleTagSelect}/>
+                  <div className="flex-grow pl-1 align-middle overflow-y-auto bg-slate-100" id="inputValue">
+                    <TagTypesVisualizer tag_types={getTagTypesFromTagArray(rowData.selectedTags)} onTagSelect={handleTagSelect}/>
                   </div>
                   <Popup trigger={<div className="flex w-1/12 h-full p-1 justify-center bg-amber-300" id="inputValue"><PencilSquareIcon className="w-5"/></div>} 
                   modal
                   >    
-                    <div className="flex flex-col relative justify-start w-[400px] max-h-[400px] top-0 left-1/3  bg-slate-600 border-2 p-1 ">
+                    <div className="flex flex-col relative justify-start w-[400px] max-h-[400px] top-0 left-1/3 bg-slate-600 border-2 p-1 ">
                       <TagBrowser onTagSelect={handleTagSelect}/>
                     </div>
                   </Popup>
@@ -160,45 +186,45 @@ export default function FilterRow({ deleteRow }: { deleteRow: () => void }){
   return(
     <div className="flex items-center justify-between w-full h-16 bg-slate-300 px-2 py-1"> 
     
-    <select 
-    name = "trackKey"
-    className="pl-1 w-1/6 h-5/6"
-    value={selectedKey}
-    onChange={(e) => setSelectedKey(e.target.value)}
-    >
-      {trackKeys.map((key,index) =>(
-        <option 
-        key={index} 
-        value={key}
-        
-        >
-          {key}
-        </option>
-      ))}
-    </select>
-    
-    <select 
-    className="pl-1 w-1/6 h-5/6"
-    value={selectedComparator}
-    onChange={(e) => setSelectedComparator(e.target.value)}
-    >
-        {(dynamicOptions[selectedKey as keyof typeof dynamicOptions] || []).map((option, index) => (
-          <option key={index} value={option}>
-            {option}
+      <select 
+      name = "trackKey"
+      className="pl-1 w-1/6 h-5/6"
+      value={rowData.selectedKey}
+      onChange={(e) => updateRow({ selectedKey: e.target.value })}
+      >
+        {trackKeys.map((key,index) =>(
+          <option 
+          key={index} 
+          value={key}
+          
+          >
+            {key}
           </option>
         ))}
-    </select>
-    
-    <div className="flex items-center justify-evenly  w-3/6 h-[90%]">
-    {
-      handleInputType()
-    }
-    </div>
+      </select>
+      
+      <select 
+      className="pl-1 w-1/6 h-5/6"
+      value={rowData.selectedComparator}
+      onChange={(e) => updateRow({ selectedComparator: e.target.value })}
+      >
+          {(dynamicOptions[rowData.selectedKey as keyof typeof dynamicOptions] || []).map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+      </select>
+      
+      <div className="flex items-center justify-evenly  w-3/6 h-[90%]">
+      {
+        handleInputType()
+      }
+      </div>
 
 
-    <button type="button" onClick={deleteRow} className="ml-2 p-1 bg-red-600 hover:bg-red-800 text-white rounded">
-        <TrashIcon className="h-4 w-4" />
-    </button>
+      <button type="button" onClick={deleteRow} className="ml-2 p-1 bg-red-600 hover:bg-red-800 text-white rounded">
+          <TrashIcon className="h-4 w-4" />
+      </button>
     </div>
   );
 }
--- src/ui/MainWindow/Filter/modalFilter.tsx
+++ src/ui/MainWindow/Filter/modalFilter.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Button } from "../../button";
+import { trackKeys } from '../../../lib/models/Track';
 import FilterRow, { AddCondition } from "./filterRow";
+import { Genre, Tag } from "../../../lib/models";
 
 let idCounter = 0;
   const generateId = () => {
@@ -10,35 +12,112 @@ let idCounter = 0;
     return returnedId;
   }
 
+const stringOptions = ['equals','contains','not equals','not contains']
+const numberOptions = ['=','<','>','<=','>=','range']
+const tagGenreOptions = ['contains all','contains some','not contains']
+
+const dynamicOptions = {
+  'File Name':stringOptions,
+  'Folder':stringOptions,
+  'Name':stringOptions,
+  'Artist':stringOptions,
+  'Length':numberOptions,
+  'BPM':numberOptions,
+  'Genres':tagGenreOptions,
+  'Tags':tagGenreOptions,
+  'Album':stringOptions,
+  'Label':stringOptions,
+  'Key':stringOptions,
+  'Date Added':numberOptions,
+  'Rating':numberOptions,
+  'Comment':stringOptions,
+  'Bitrate':numberOptions
+  };
+
 export default function ModalFilter(){
 
-  
-  const [filterRows, setFilterRows] = useState<JSX.Element[]>([]);
+  const [allConditions, setAllConditions] = useState<boolean>(true);
+  const [filterRowsData, setFilterRowsData] = useState<
+    {
+      id: number;
+      selectedKey: string;
+      selectedComparator: string;
+      selectedTags: Tag[];
+      selectedGenres: Genre[];
+      inputValue: any;
+      inputValueMin: any;
+      inputValueMax: any;
+    }[]
+  >([]);
 
   const addFilterRow = () => {
     const id = generateId();
-    setFilterRows([...filterRows,  <FilterRow key={id} deleteRow={() => deleteFilterRow(id)}/>]);
+    setFilterRowsData([
+      ...filterRowsData,
+      {
+        id,
+        selectedKey: trackKeys[0],
+        selectedComparator: stringOptions[0],
+        selectedTags: [],
+        selectedGenres: [],
+        inputValue: "",
+        inputValueMin: "",
+        inputValueMax: ""
+      },
+    ]);
   };
 
   const deleteFilterRow = (id: number) => {
-    console.log("Deleting row with id",id);
-    setFilterRows((prevRows) => prevRows.filter(row => Number(row.key) != id));
+    setFilterRowsData((prevRowsData) =>
+      prevRowsData.filter((rowData) => rowData.id !== id)
+    );
   };
 
-  // const form = document.querySelector('filterForm'); 
-     
-  // form.addEventListener('submit', function(event) { 
-  //   event.preventDefault(); 
-  //   // Your form submission logic here 
-  // }); 
+  const updateFilterRow = (
+    id: number,
+    updatedRowData: Partial<{
+      selectedKey: string;
+      selectedComparator: string;
+      selectedTags: Tag[];
+      selectedGenres: Genre[];
+      inputValue: any;
+      inputValueMin: any;
+      inputValueMax: any;
+    }>
+  ) => {
+    setFilterRowsData((prevRowsData) =>
+      prevRowsData.map((rowData) =>
+        rowData.id === id ? { ...rowData, ...updatedRowData } : rowData
+      )
+    );
+  };
+
+  const handleConditionalChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = event.target;
+    if (value == "AND"){
+      setAllConditions(true)
+    }
+    else if (value == "OR"){
+      setAllConditions(false)
+    }
+  }
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("Filter data:", filterRowsData);
+    console.log("ALL Conditions:",allConditions)
+    // Your form submission logic here
+  };
 
   return(
-    <form id='filterForm' className="flex flex-col fixed w-1/2 h-1/2 top-1/4 left-1/4 bg-slate-700 border-4 border-slate-700 rounded-sm">
+    <form id='filterForm' 
+    className="flex flex-col fixed w-1/2 h-1/2 top-1/4 left-1/4 bg-slate-700 border-4 border-slate-700 rounded-sm"
+    onSubmit={handleSubmit}>
         <div className="flex h-10 w-full bg-slate-300 items-center justify-between">
         <div className="text-xl p-2">ADVANCED FILTER OPTIONS</div>
         <div className="flex items-center h-full gap-2 pl-2 bg-amber-300">
           FULFILL
-          <select name="ConditionLogic" className="flex h-full pl-2 bg-amber-100">
+          <select name="ConditionLogic" className="flex h-full pl-2 bg-amber-100" onChange={handleConditionalChange}>
             <option value="AND">
               ALL CONDITIONS
             </option>
@@ -49,7 +128,14 @@ export default function ModalFilter(){
         </div>
         </div>
         <span className="flex flex-grow flex-col gap-3 m-3 mt-6 bg-slate-500 bg-gradient-to-b p-4 overflow-y-auto">
-          {filterRows}
+          {filterRowsData.map((rowData) => (
+            <FilterRow
+              key={rowData.id}
+              rowData={rowData}
+              deleteRow={() => deleteFilterRow(rowData.id)}
+              updateRow={(updatedData) => updateFilterRow(rowData.id, updatedData)}
+            />
+          ))}
           <AddCondition onClick={addFilterRow}/>
         </span>
         <Button type="submit">Apply Filter</Button>
